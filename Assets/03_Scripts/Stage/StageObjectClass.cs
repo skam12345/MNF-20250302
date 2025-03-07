bc37da03285bc43b4f40c5c04b58f9eff080d63f
skam12345 <skam1234561@gmail.com>
2025-03-07 21:49:50 +0900

    오류 수정
    오류수정

--- Assets/03_Scripts/Stage/StageObjectClass.cs
+++ Assets/03_Scripts/Stage/StageObjectClass.cs
@@ -1,8 +1,23 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.AI;
 
-public class StageObjectClass : MonoBehaviour
+[System.Serializable]
+public class StageObjectClass
 {
-    []
+    public int idx;
+    public int stageNum;
+    public int subNum;
+    public string dungeonName;
+    public int rewardExp;
+    public int rewardGold;
+    public string difficulty;
+    public int timer;
+    public int monster1;
+    public int monster2;
+    public int monster3;
+    public int monster4;
+    public int bossidx;
+    public string description;
+
+
 }
