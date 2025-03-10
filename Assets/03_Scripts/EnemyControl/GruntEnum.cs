fd32826c981fdd3a8cce992a63903e269080a6d1
skam12345 <skam1234561@gmail.com>
2025-03-10 22:47:45 +0900

    적 이동 로직 모션 추가
    적 이동 로직 모션 추가

---
+++ Assets/03_Scripts/EnemyControl/GruntEnum.cs
@@ -0,0 +1,15 @@
+
+
+public class GruntEnum 
+{ 
+    public enum GRUNT_ANIMATION:int
+    {
+        ATTACK02 = 0,
+        DIE,
+        GETHIT,
+        IDLE,
+        RUN,
+        VICTORY,
+        WALK,
+    };
+}
