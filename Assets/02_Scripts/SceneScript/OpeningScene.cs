fb430564fa82d23ad01cc13d4ff7980a70f8e5ea
skam12345 <skam1234561@gmail.com>
2025-05-19 23:32:19 +0900

    20250519 작업

---
+++ Assets/02_Scripts/SceneScript/OpeningScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningScene : MonoBehaviour
+{
+    [SerializeField] UITextBox textBox;
+    [SerializeField] GameObject conversation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        conversation.SetActive(true);
+        textBox.OnInit("Opening");
+        textBox.PlayText();
+    }
+}
